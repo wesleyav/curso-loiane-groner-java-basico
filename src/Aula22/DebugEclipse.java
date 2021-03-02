dbe394885cfe6f29b46c59160f20a442412099d2
Wesley Araujo Vieira <wesley.contato@hotmail.com>
2021-03-02 15:34:37 -0300

    Aula 22 - Debug

---
+++ src/Aula22/DebugEclipse.java
@@ -0,0 +1,14 @@
+package Aula22;
+
+public class DebugEclipse {
+
+	public static void main(String[] args) {
+
+		int[] notas = new int[3];
+
+		for (int i = 0; i < 4; i++) {
+			notas[i] = i;
+		}
+	}
+
+}
