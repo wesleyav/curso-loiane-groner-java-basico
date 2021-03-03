fdbc9b8d3af854e6271d06319f7d426e2a5484a0
Wesley Araujo Vieira <wesley.contato@hotmail.com>
2021-03-03 19:40:21 -0300

    Aula 31 - Modificadores private e public

---
+++ src/Aula31/Carro.java
@@ -0,0 +1,31 @@
+package Aula31;
+
+ class Carro {
+
+	String marca;
+	String modelo;
+	int numPassageiros;
+	double capCombustivel;
+	double consumoCombustivel;
+
+	public void exibirAutonomia() {
+		System.out.println("A autonomia do carro é: " + this.capCombustivel
+				+ this.capCombustivel + " km");
+	}
+
+	public double obterAutonomia() {
+		System.out.println("Método obterAutonomia foi chamado.");
+		return this.capCombustivel * this.consumoCombustivel;
+	}
+	
+	private double divideKMPorConsumoCombustivel(double km){
+		return km/this.consumoCombustivel;
+	}
+
+	public double calcularCombustivel(double km) {
+
+		return this.divideKMPorConsumoCombustivel(km);
+		
+	}
+
+}
