316d588de041ae4ba289fcab112244663f4eb6b9
Wesley Araujo Vieira <wesley.contato@hotmail.com>
2021-03-03 19:52:22 -0300

    Aula 32 - Encapsulamento: métodos getters e setters

---
+++ src/Aula32/TesteCarro.java
@@ -0,0 +1,13 @@
+package Aula32;
+
+public class TesteCarro {
+
+	public static void main(String[] args) {
+
+		Carro van = new Carro();
+		
+		van.setMarca("Fiat");
+		System.out.println(van.getMarca());
+	}
+
+}
