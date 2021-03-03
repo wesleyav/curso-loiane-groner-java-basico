6e312f4bb071860f20d16e480ef74d4f367933ee
Wesley Araujo Vieira <wesley.contato@hotmail.com>
2021-03-03 09:20:01 -0300

    Aula 26 - Classes e métodos com retorno

---
+++ src/Aula26/Carro.java
@@ -0,0 +1,19 @@
+package Aula26;
+
+public class Carro {
+	String marca;
+	String modelo;
+	int numPassageiros;
+	double capCombustivel;
+	double consumoCombustivel;
+
+	void exibirAutonomia() {
+		System.out.println("A autonomia do carro é: " + +capCombustivel
+				* consumoCombustivel + " km");
+	}
+
+	double obterAutonomia() {
+		System.out.println("Método obterAutonomia foi chamado.");
+		return capCombustivel * consumoCombustivel;
+	}
+}
