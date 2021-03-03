cb44c735aa4b44a54089feacbae96b15db8db87c
Wesley Araujo Vieira <wesley.contato@hotmail.com>
2021-03-03 19:02:29 -0300

    Aula 30 - Palavra chave this

---
+++ src/Aula30/TesteCarro.java
@@ -0,0 +1,14 @@
+package Aula30;
+
+public class TesteCarro {
+
+	public static void main(String[] args) {
+		
+		
+		Carro van = new Carro("Fiat", "Ducato");
+		
+		System.out.println(van.numPassageiros);
+
+	}
+
+}
