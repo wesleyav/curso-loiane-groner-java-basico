b616d5ead42d574f4a8fa01bda45cbaa270c78de
Wesley Araujo Vieira <wesley.contato@hotmail.com>
2021-03-03 09:07:37 -0300

    Aula 25 - Classes e métodos simples

---
+++ src/Aula25/Carro.java
@@ -0,0 +1,15 @@
+package Aula25;
+
+public class Carro {
+
+	String marca;
+	String modelo;
+	int numPassageiros;
+	double capCombustivel;
+	double consumoCombustivel;
+
+	void exibirAutonomia() {
+		System.out.println("A autonomia do carro é: " + +capCombustivel
+				* consumoCombustivel + " km");
+	}
+}
---
+++ src/Aula25/TesteCarro.java
@@ -0,0 +1,22 @@
+package Aula25;
+
+import Aula25.Carro;
+
+public class TesteCarro {
+
+	public static void main(String[] args) {
+		Carro van = new Carro();
+		van.marca = "Fiat";
+		van.modelo = "Ducato";
+		van.numPassageiros = 10;
+		van.capCombustivel = 100;
+		van.consumoCombustivel = 0.2;
+
+		System.out.println(van.marca);
+		System.out.println(van.modelo);
+		
+		van.exibirAutonomia();
+
+	}
+
+}
