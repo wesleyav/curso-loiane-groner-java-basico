e0cc390f852e6c381aea4b76d0265c3ff1ce6181
Wesley Araujo Vieira <wesley.contato@hotmail.com>
2021-12-21 19:59:18 -0300

    Aula 40 - Herança e Polimorfismo: sobrecarga de métodos

---
+++ src/Aula40/Aluno.java
@@ -0,0 +1,51 @@
+package Aula40;
+
+public class Aluno extends Pessoa {
+
+	private String curso;
+	private Double[] notas;
+
+	public Aluno() {
+		super();
+	}
+
+	public Aluno(String nome, String endereco, String telefone) {
+		super(nome, endereco, telefone);
+		this.curso = curso;
+	}
+
+	public String getCurso() {
+		return curso;
+	}
+
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+
+	public Double[] getNotas() {
+		return notas;
+	}
+
+	public void setNotas(Double[] notas) {
+		this.notas = notas;
+	}
+
+	public double calcularMedia() {
+		return 0;
+	}
+
+	public boolean verificarAprovado() {
+		return true;
+	}
+
+	public void metodoQualquer() {
+		super.setCpf("99999999999");
+	}
+
+	public String obterEtiquetaEndereco() {
+		String s = "Endereço do Aluno: ";
+		s += super.getEndereco();
+		return s;
+	}
+
+}
---
+++ src/Aula40/Teste.java
@@ -0,0 +1,21 @@
+package Aula40;
+
+public class Teste {
+
+	public static void main(String[] args) {
+
+		Pessoa pessoa = new Pessoa();
+		Pessoa aluno = new Aluno();
+		Pessoa professor = new Professor();
+
+		pessoa.setEndereco("Rua 1, numero 1");
+		aluno.setEndereco("Rua 2, numero 2");
+		professor.setEndereco("Rua 3, numero 3");
+
+		System.out.println(pessoa.obterEtiquetaEndereco());
+		System.out.println(aluno.obterEtiquetaEndereco());
+		System.out.println(professor.obterEtiquetaEndereco());
+
+	}
+
+}
