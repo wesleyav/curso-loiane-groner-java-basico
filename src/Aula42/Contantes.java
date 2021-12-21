a728c650acad706cba0f0cd51c8a766e4f015d6e
Wesley Araujo Vieira <wesley.contato@hotmail.com>
2021-12-21 20:44:25 -0300

    Aula 42 - Palavra chave final

---
+++ src/Aula42/Contantes.java
@@ -0,0 +1,10 @@
+package Aula42;
+
+public final class Contantes {
+
+	public static final String URL_BLOG = "http://loiane.com";
+	public static final String CURSO_COMPLETO = "http://loiane.training.com";
+
+	private String urlBlog;
+
+}
---
+++ src/Aula42/Teste.java
@@ -0,0 +1,18 @@
+package Aula42;
+
+public class Teste {
+
+	public static void main(String[] args) {
+
+		Pessoa pessoa = new Pessoa();
+		Aluno aluno = new Aluno();
+		Professor professor = new Professor();
+
+		System.out.println(Contantes.URL_BLOG);
+
+		final int TOTAL = 1;
+
+		System.out.println(TOTAL);
+	}
+
+}
