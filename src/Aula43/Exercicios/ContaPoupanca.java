8b84a3b64aa201b8649f25cae7a84a17fddddd1b
Wesley Araujo Vieira <wesley.contato@hotmail.com>
2021-12-22 19:37:47 -0300

    Exercicios Aula 43 - Parte 1

---
+++ src/Aula43/Exercicios/ContaPoupanca.java
@@ -0,0 +1,34 @@
+package Aula43.Exercicios;
+
+import java.util.Calendar;
+
+public class ContaPoupanca extends ContaBancaria {
+
+	private int diaRendimento;
+
+	public int getDiaRendimento() {
+		return diaRendimento;
+	}
+
+	public void setDiaRendimento(int diaRendimento) {
+		this.diaRendimento = diaRendimento;
+	}
+
+	@Override
+	public String toString() {
+		String s = "ContaPoupança{";
+		s += " diaRendimento " + diaRendimento;
+		s += ";" + super.toString();
+		s += "}";
+		return s;
+	}
+
+	public boolean calcularNovoSaldo(double taxaRendimento) {
+		Calendar hoje = Calendar.getInstance();
+		if (diaRendimento == hoje.get(Calendar.DAY_OF_MONTH)) {
+			this.setSaldo(this.getSaldo() + (this.getSaldo() * taxaRendimento));
+			return true;
+		}
+		return false;
+	}
+}
